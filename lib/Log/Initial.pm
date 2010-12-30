279cac64fd4568645bf985b20ec070de65612adf
Yuji Kawamoto <yukawamoto@gmail.com>
2010-12-30 20:09:43 +0900

    initial commit

---
+++ lib/Log/Initial.pm
@@ -0,0 +1,32 @@
+package Log::Initial;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Log::Initial -
+
+=head1 SYNOPSIS
+
+  use Log::Initial;
+
+=head1 DESCRIPTION
+
+Log::Initial is
+
+=head1 AUTHOR
+
+Yuji Kawamoto E<lt>yukawamoto at gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
