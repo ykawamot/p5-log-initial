cc01aa9fc3c0b4ed0b001e7b9fad6975516d130f
Yuji Kawamoto <yukawamoto@gmail.com>
2010-12-31 22:57:32 +0900

    ver 0.01

--- lib/Log/Initial.pm
+++ lib/Log/Initial.pm
@@ -1,7 +1,49 @@
 package Log::Initial;
+
 use strict;
 use warnings;
+use Data::Dumper;
+
+require Exporter;
+
 our $VERSION = '0.01';
+our @ISA     = qw(Exporter);
+our @EXPORT  = qw(D I W E F);
+
+our $DEBUG = 0;
+our $FH    = *STDOUT;
+
+sub D { _log(@_) if $DEBUG; }
+sub I { _log(@_); }
+sub W { _log(@_); }
+sub E { _log(@_); }
+sub F { _log(@_); exit 1; }
+
+sub _log {
+    local $Data::Dumper::Indent   = 1;
+    local $Data::Dumper::Sortkeys = 1;
+    local $Data::Dumper::Terse    = 1;
+    
+    my ($F, $L, $S) = (caller 1)[1..3];
+
+    $F =~ s#.*/##;
+    $S =~ s#.*::##;
+
+    my $buf = '';
+
+    for (@_) {
+        if (ref) {
+            $_ = Dumper $_;
+        }
+        $buf .= sprintf(
+            "[%s] [%s] %s(%d): %s\n",
+            scalar localtime,
+            $S, $F, $L, $_,
+        ) for (split /\n/);
+    }
+
+    print {$FH} $buf;
+}
 
 1;
 __END__
